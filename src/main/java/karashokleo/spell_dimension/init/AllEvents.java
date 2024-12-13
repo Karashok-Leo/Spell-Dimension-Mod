c6c021c2d09f0dea53d1502a87cf38195f53a6aa
Karashok-Leo <1010177289@qq.com>
2024-12-13 16:25:00 +0800

    optimize adapting trait

--- src/main/java/karashokleo/spell_dimension/init/AllEvents.java
+++ src/main/java/karashokleo/spell_dimension/init/AllEvents.java
@@ -1,6 +1,7 @@
 package karashokleo.spell_dimension.init;
 
 import com.obscuria.aquamirae.registry.AquamiraeItems;
+import io.github.fabricators_of_create.porting_lib.entity.events.living.LivingHurtEvent;
 import karashokleo.l2hostility.content.component.mob.MobDifficulty;
 import karashokleo.l2hostility.content.trait.common.AdaptingTrait;
 import karashokleo.l2hostility.init.LHTraits;
@@ -17,12 +18,14 @@ import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.MobSpawnerBlockEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.TypedActionResult;
 import net.spell_power.api.SpellDamageSource;
+import net.spell_power.api.SpellPowerTags;
 
 import java.util.Optional;
 
@@ -88,6 +91,8 @@ public class AllEvents
         adaptiveCompat();
     }
 
+    private static final String ADAPTIVE_CACHE = "[SpellCache]";
+
     private static void adaptiveCompat()
     {
         SpellImpactEvents.BEFORE.register((world, caster, targets, spellInfo) ->
@@ -100,8 +105,29 @@ public class AllEvents
                 AdaptingTrait.Data data = diff.getOrCreateData(LHTraits.ADAPTIVE.getId(), AdaptingTrait.Data::new);
                 int adaptLv = diff.getTraitLevel(LHTraits.ADAPTIVE);
                 if (adaptLv > 0)
-                    data.adapt("[SpellCache]" + spellInfo.id().toString(), adaptLv);
+                    data.adapt(ADAPTIVE_CACHE + spellInfo.id().toString(), adaptLv);
             }
         });
+    }
+
+    /**
+     * @return true to cancel further processing
+     */
+    public static boolean adaptSpell(int level, LivingEntity entity, LivingHurtEvent event)
+    {
+        if (event.getSource().isIn(SpellPowerTags.DamageType.ALL))
+        {
+            var cap = MobDifficulty.get(entity);
+            if (cap.isEmpty()) return false;
+
+            AdaptingTrait.Data data = cap.get().getOrCreateData(LHTraits.ADAPTIVE.getId(), AdaptingTrait.Data::new);
+            Optional<String> firstToken = data.memory.stream().filter(string -> string.startsWith(ADAPTIVE_CACHE)).findFirst();
+            if (firstToken.isEmpty()) return false;
+
+            data.adapt(firstToken.get(), level)
+                    .ifPresent(factor -> event.setAmount(event.getAmount() * factor));
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/main/java/karashokleo/spell_dimension/mixin/AdaptingTraitMixin.java
@@ -0,0 +1,25 @@
+package karashokleo.spell_dimension.mixin;
+
+import io.github.fabricators_of_create.porting_lib.entity.events.living.LivingHurtEvent;
+import karashokleo.l2hostility.content.trait.common.AdaptingTrait;
+import karashokleo.spell_dimension.init.AllEvents;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AdaptingTrait.class)
+public abstract class AdaptingTraitMixin
+{
+    @Inject(
+            method = "onHurt",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void inject_onHurt(int level, LivingEntity entity, LivingHurtEvent event, CallbackInfo ci)
+    {
+        if (AllEvents.adaptSpell(level, entity, event))
+            ci.cancel();
+    }
+}
