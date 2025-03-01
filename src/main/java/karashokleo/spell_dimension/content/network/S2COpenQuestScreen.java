4ba08c87baf057ddccaefb2888461ec349b71db5
Karashok-Leo <1010177289@qq.com>
2025-03-01 20:08:59 +0800

    Fix S2COpenQuestScreen, but why?

--- src/main/java/karashokleo/spell_dimension/content/network/S2COpenQuestScreen.java
+++ src/main/java/karashokleo/spell_dimension/content/network/S2COpenQuestScreen.java
@@ -5,12 +5,15 @@ import dev.xkmc.l2serial.serialization.SerialClass;
 import karashokleo.l2hostility.client.L2HostilityClient;
 import karashokleo.spell_dimension.api.quest.QuestUsage;
 import karashokleo.spell_dimension.client.screen.QuestScreen;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.Hand;
 
 @SerialClass
 public record S2COpenQuestScreen(Hand hand) implements SerialPacketS2C
 {
+    @Environment(EnvType.CLIENT)
     @Override
     public void handle(ClientPlayerEntity player)
     {
