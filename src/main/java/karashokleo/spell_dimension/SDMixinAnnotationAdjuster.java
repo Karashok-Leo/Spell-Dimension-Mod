f63ad6e4e27a35a474b14e87114da9687ca47116
Karashok-Leo <1010177289@qq.com>
2025-03-24 19:28:13 +0800

    Fix too much particles (caused by Rainglow)

--- src/main/java/karashokleo/spell_dimension/SDMixinAnnotationAdjuster.java
+++ src/main/java/karashokleo/spell_dimension/SDMixinAnnotationAdjuster.java
@@ -1,9 +1,11 @@
 package karashokleo.spell_dimension;
 
 import com.bawnorton.mixinsquared.adjuster.tools.AdjustableAnnotationNode;
+import com.bawnorton.mixinsquared.adjuster.tools.AdjustableInjectNode;
 import com.bawnorton.mixinsquared.api.MixinAnnotationAdjuster;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import org.objectweb.asm.tree.MethodNode;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import java.util.List;
 
@@ -12,9 +14,22 @@ public class SDMixinAnnotationAdjuster implements MixinAnnotationAdjuster
     @Override
     public AdjustableAnnotationNode adjust(List<String> targetClassNames, String mixinClassName, MethodNode handlerNode, AdjustableAnnotationNode annotationNode)
     {
-        if (!mixinClassName.equals("io.github.fabricators_of_create.porting_lib.loot.mixin.loottable.LootTableMixin"))
-            return annotationNode;
-        if (!annotationNode.is(WrapMethod.class)) return annotationNode;
-        return null;
+        if (mixinClassName.equals("io.github.fabricators_of_create.porting_lib.loot.mixin.loottable.LootTableMixin") &&
+            annotationNode.is(WrapMethod.class)
+        )
+        {
+            return null;
+        }
+        if (mixinClassName.equals("io.ix0rai.rainglow.mixin.SlimeEntityMixin") &&
+            annotationNode.is(Inject.class) &&
+            annotationNode.as(AdjustableInjectNode.class)
+                    .getMethod()
+                    .get(0)
+                    .equals("tick")
+        )
+        {
+            return null;
+        }
+        return annotationNode;
     }
 }
