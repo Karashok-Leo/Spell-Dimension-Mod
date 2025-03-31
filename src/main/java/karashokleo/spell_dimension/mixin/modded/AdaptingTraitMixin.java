f2b8acca9e1fc6dfa47843b71a6d0c929c5cb849
Karashok-Leo <1010177289@qq.com>
2025-03-31 23:34:13 +0800

    Fix AdaptingTrait
    update l2h

--- src/main/java/karashokleo/spell_dimension/mixin/modded/AdaptingTraitMixin.java
+++ src/main/java/karashokleo/spell_dimension/mixin/modded/AdaptingTraitMixin.java
@@ -1,5 +1,6 @@
 package karashokleo.spell_dimension.mixin.modded;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import io.github.fabricators_of_create.porting_lib.entity.events.living.LivingHurtEvent;
 import karashokleo.l2hostility.content.component.mob.MobDifficulty;
 import karashokleo.l2hostility.content.trait.common.AdaptingTrait;
@@ -10,17 +11,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AdaptingTrait.class)
+@Mixin(value = AdaptingTrait.class, remap = false)
 public abstract class AdaptingTraitMixin
 {
     @Inject(
             method = "onHurt",
-            at = @At("HEAD"),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lkarashokleo/l2hostility/content/trait/common/AdaptingTrait$Data;adapt(Ljava/lang/String;I)Ljava/util/Optional;"
+            ),
             cancellable = true
     )
-    private void inject_onHurt(MobDifficulty difficulty, LivingEntity entity, int level, LivingHurtEvent event, CallbackInfo ci)
+    private void inject_onHurt(MobDifficulty difficulty, LivingEntity entity, int level, LivingHurtEvent event, CallbackInfo ci, @Local AdaptingTrait.Data data)
     {
-        if (AllEvents.adaptSpell(level, entity, event))
+        if (AllEvents.adaptSpell(level, entity, event, data))
             ci.cancel();
     }
 }
