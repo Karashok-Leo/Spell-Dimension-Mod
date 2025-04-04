8c5f405b1906446bfa9c3454ae8d49baab43872e
Karashok-Leo <1010177289@qq.com>
2025-04-04 11:33:01 +0800

    Add floating NirvanaStarfall when trigger

--- src/main/java/karashokleo/spell_dimension/content/item/trinket/NirvanaStarfallItem.java
+++ src/main/java/karashokleo/spell_dimension/content/item/trinket/NirvanaStarfallItem.java
@@ -4,7 +4,9 @@ import karashokleo.l2hostility.content.item.trinket.core.DamageListenerTrinket;
 import karashokleo.l2hostility.content.item.trinket.core.SingleEpicTrinketItem;
 import karashokleo.l2hostility.util.EffectHelper;
 import karashokleo.leobrary.effect.api.util.EffectUtil;
+import karashokleo.spell_dimension.content.network.S2CFloatingItem;
 import karashokleo.spell_dimension.data.SDTexts;
+import karashokleo.spell_dimension.init.AllPackets;
 import karashokleo.spell_dimension.init.AllStatusEffects;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
@@ -13,7 +15,9 @@ import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.DamageTypeTags;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import net.spell_power.api.SpellPower;
 import net.spell_power.api.SpellSchools;
@@ -45,8 +49,15 @@ public class NirvanaStarfallItem extends SingleEpicTrinketItem implements Damage
 
         EffectHelper.forceAddEffectWithEvent(entity, new StatusEffectInstance(AllStatusEffects.NIRVANA, DURATION, level), entity);
 
+        if (entity instanceof ServerPlayerEntity player)
+        {
+            S2CFloatingItem floatingItem = new S2CFloatingItem(stack.copy());
+            AllPackets.toClientPlayer(player, floatingItem);
+        }
+
         entity.setHealth(entity.getMaxHealth());
         EffectUtil.streamEffects(entity, StatusEffectCategory.HARMFUL)
+                .toList()
                 .forEach(entity::removeStatusEffect);
 
         return false;
@@ -58,8 +69,8 @@ public class NirvanaStarfallItem extends SingleEpicTrinketItem implements Damage
         tooltip.add(SDTexts.TOOLTIP$NIRVANA_STARFALL$DEFRAUDING_REAPER.get(
                 SPELL_POWER_THRESHOLD,
                 DURATION / 20
-        ));
-        tooltip.add(SDTexts.TOOLTIP$NIRVANA_STARFALL$NIRVANA_REBIRTH.get());
+        ).formatted(Formatting.DARK_RED));
+        tooltip.add(SDTexts.TOOLTIP$NIRVANA_STARFALL$NIRVANA_REBIRTH.get().formatted(Formatting.DARK_RED));
         super.appendTooltip(stack, world, tooltip, context);
     }
 }
