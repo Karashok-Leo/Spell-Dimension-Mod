71ee70a4927ca33d3667750de5bb9feb0b4291e6
Karashok-Leo <1010177289@qq.com>
2025-06-28 19:39:16 +0800

    Fix tracked range of mobs from Deeper Darker: 10 blocks->10 chunks

---
+++ src/main/java/karashokleo/spell_dimension/mixin/modded/DDEntitiesMixin.java
@@ -0,0 +1,24 @@
+package karashokleo.spell_dimension.mixin.modded;
+
+import com.kyanite.deeperdarker.content.DDEntities;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = DDEntities.class, remap = false)
+public abstract class DDEntitiesMixin
+{
+    @Redirect(
+            method = "<clinit>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/fabricmc/fabric/api/object/builder/v1/entity/FabricEntityTypeBuilder;trackRangeBlocks(I)Lnet/fabricmc/fabric/api/object/builder/v1/entity/FabricEntityTypeBuilder;"
+            )
+    )
+    private static <T extends Entity> FabricEntityTypeBuilder<T> redirect_trackRangeBlocks(FabricEntityTypeBuilder<T> instance, int range)
+    {
+        return instance.trackRangeChunks(range);
+    }
+}
