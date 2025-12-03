22059048e7cd50767cadc66d9a979659ac6eea78
Karashok-Leo <1010177289@qq.com>
2025-12-03 15:27:52 +0800

    Fix player base health

--- src/main/java/karashokleo/spell_dimension/content/event/PlayerHealthEvents.java
+++ src/main/java/karashokleo/spell_dimension/content/event/PlayerHealthEvents.java
@@ -93,7 +93,7 @@ public class PlayerHealthEvents
         {
             return;
         }
-        if (display.shouldAnnounceToChat())
+        if (!display.shouldAnnounceToChat())
         {
             return;
         }
