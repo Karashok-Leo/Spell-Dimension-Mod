b3d529c1b91820539e744837c1d4af8c09ce6997
Karashok-Leo <1010177289@qq.com>
2025-12-15 12:20:41 +0800

    Fix crash when ticking buffs

--- src/main/java/karashokleo/spell_dimension/content/component/BuffComponentImpl.java
+++ src/main/java/karashokleo/spell_dimension/content/component/BuffComponentImpl.java
@@ -22,10 +22,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-
 public record BuffComponentImpl(
-        LivingEntity owner,
-        Map<BuffType<?>, BuffEntry<?>> buffMap
+    LivingEntity owner,
+    Map<BuffType<?>, BuffEntry<?>> buffMap
 ) implements BuffComponent
 {
     private static final String ENTRIES_KEY = "Entries";
@@ -40,15 +39,22 @@ public record BuffComponentImpl(
     public <B extends Buff> Optional<B> get(BuffType<B> type)
     {
         if (this.buffMap.get(type) != null)
+        {
             return Optional.of((B) this.buffMap.get(type).buff);
-        else return Optional.empty();
+        } else
+        {
+            return Optional.empty();
+        }
     }
 
     @Override
     public <B extends Buff> void apply(BuffType<B> type, B buff, @Nullable LivingEntity source)
     {
         BuffEntry<?> remove = this.buffMap.remove(type);
-        if (remove != null) remove.buff.onRemoved(this.owner, remove.source);
+        if (remove != null)
+        {
+            remove.buff.onRemoved(this.owner, remove.source);
+        }
         this.buffMap.put(type, new BuffEntry<>(type, buff, source));
         buff.onApplied(this.owner, source);
     }
@@ -58,15 +64,20 @@ public record BuffComponentImpl(
     {
         BuffEntry<?> buffEntry = this.buffMap.remove(type);
         if (buffEntry != null)
+        {
             buffEntry.buff.onRemoved(this.owner, buffEntry.source);
+        }
     }
 
     @Override
     public void serverTick()
     {
         AllComponents.BUFF.sync(this.owner);
-        for (BuffEntry<?> entry : this.buffMap.values())
+        var snapshot = List.copyOf(this.buffMap.values());
+        for (BuffEntry<?> entry : snapshot)
+        {
             entry.buff.serverTick(owner, entry.source);
+        }
     }
 
     @Override
@@ -74,11 +85,16 @@ public record BuffComponentImpl(
     {
         this.buffMap.clear();
         for (NbtElement element : tag.getList(ENTRIES_KEY, NbtElement.COMPOUND_TYPE))
+        {
             if (element instanceof NbtCompound compound)
             {
                 BuffEntry<?> entry = BuffEntry.readFromNbt(compound, owner.getWorld());
-                if (entry != null) this.buffMap.put(entry.type, entry);
+                if (entry != null)
+                {
+                    this.buffMap.put(entry.type, entry);
+                }
             }
+        }
     }
 
     @Override
@@ -134,9 +150,11 @@ public record BuffComponentImpl(
                     B buff = buffResult.get().getFirst();
                     LivingEntity source = null;
                     if (world instanceof ServerWorld serverWorld &&
-                            tag.containsUuid(SOURCE_KEY) &&
-                            serverWorld.getEntity(tag.getUuid(SOURCE_KEY)) instanceof LivingEntity living)
+                        tag.containsUuid(SOURCE_KEY) &&
+                        serverWorld.getEntity(tag.getUuid(SOURCE_KEY)) instanceof LivingEntity living)
+                    {
                         source = living;
+                    }
                     return new BuffEntry<>(buffType, buff, source);
                 }
             }
@@ -150,7 +168,9 @@ public record BuffComponentImpl(
             Optional<NbtElement> buffResult = this.type.codec().encodeStart(NbtOps.INSTANCE, this.buff).result();
             buffResult.ifPresent(nbtElement -> tag.put(BUFF_KEY, nbtElement));
             if (source != null)
+            {
                 tag.putUuid(SOURCE_KEY, source.getUuid());
+            }
         }
 
         @SuppressWarnings("unchecked")
@@ -160,8 +180,10 @@ public record BuffComponentImpl(
             B buff = buf.decodeAsJson(buffType.codec());
             LivingEntity source = null;
             if (buf.readBoolean() &&
-                    world.getEntityById(buf.readInt()) instanceof LivingEntity living)
+                world.getEntityById(buf.readInt()) instanceof LivingEntity living)
+            {
                 source = living;
+            }
             return new BuffEntry<>(buffType, buff, source);
         }
 
@@ -170,7 +192,10 @@ public record BuffComponentImpl(
             buf.encodeAsJson(BuffTypeRegistry.BUFF_TYPE_REGISTRY.getCodec(), this.type);
             buf.encodeAsJson(this.type.codec(), this.buff);
             buf.writeBoolean(this.source != null);
-            if (this.source != null) buf.writeInt(this.source.getId());
+            if (this.source != null)
+            {
+                buf.writeInt(this.source.getId());
+            }
         }
     }
 }
