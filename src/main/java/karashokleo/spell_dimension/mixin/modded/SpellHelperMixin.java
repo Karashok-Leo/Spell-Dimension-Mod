27f67536e890c4ad4b637c62455d74df60ae4983
Karashok-Leo <1010177289@qq.com>
2026-01-13 16:14:56 +0800

    Fix SpellImpactEvents.POST

--- src/main/java/karashokleo/spell_dimension/mixin/modded/SpellHelperMixin.java
+++ src/main/java/karashokleo/spell_dimension/mixin/modded/SpellHelperMixin.java
@@ -1,5 +1,7 @@
 package karashokleo.spell_dimension.mixin.modded;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.sugar.Local;
 import io.github.fabricators_of_create.porting_lib.entity.events.living.MobEffectEvent;
 import karashokleo.l2hostility.util.EffectHelper;
@@ -15,6 +17,7 @@ import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import net.spell_engine.api.spell.Spell;
@@ -29,6 +32,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Collection;
 import java.util.List;
 
 @Mixin(SpellHelper.class)
@@ -66,18 +70,22 @@ public abstract class SpellHelperMixin
             target = "Lnet/spell_engine/internals/SpellHelper$ImpactContext;<init>(FFLnet/minecraft/util/math/Vec3d;Lnet/spell_power/api/SpellPower$Result;Lnet/spell_engine/utils/TargetHelper$TargetingMode;)V"
         )
     )
-    private static void inject_performSpell_before(World world, PlayerEntity player, Identifier spellId, List<Entity> targets, SpellCast.Action action, float progress, CallbackInfo ci, @Local SpellInfo spellInfo)
+    private static void inject_performSpell(World world, PlayerEntity player, Identifier spellId, List<Entity> targets, SpellCast.Action action, float progress, CallbackInfo ci, @Local SpellInfo spellInfo)
     {
         SpellImpactEvents.PRE.invoker().invoke(world, player, targets, spellInfo);
     }
 
-    @Inject(
-        method = "performImpacts(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/Entity;Lnet/spell_engine/api/spell/SpellInfo;Lnet/spell_engine/internals/SpellHelper$ImpactContext;Z)Z",
-        at = @At("RETURN")
+    @WrapMethod(
+        method = "performImpact"
     )
-    private static void inject_performImpacts(World world, LivingEntity caster, Entity target, Entity aoeSource, SpellInfo spellInfo, SpellHelper.ImpactContext context, boolean additionalTargetLookup, CallbackInfoReturnable<Boolean> cir)
+    private static boolean wrap_performImpact(World world, LivingEntity caster, Entity target, SpellInfo spellInfo, Spell.Impact impact, SpellHelper.ImpactContext context, Collection<ServerPlayerEntity> trackers, Operation<Boolean> original)
     {
-        SpellImpactEvents.POST.invoker().invoke(world, caster, List.of(target), spellInfo);
+        boolean success = original.call(world, caster, target, spellInfo, impact, context, trackers);
+        if (success)
+        {
+            SpellImpactEvents.POST.invoker().invoke(world, caster, List.of(target), spellInfo);
+        }
+        return success;
     }
 
     @Inject(
